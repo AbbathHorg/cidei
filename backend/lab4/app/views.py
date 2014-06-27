21537df0a1cfa0731b877cd33941950c9f3cfb56
victor hernandez <vh51028@gmail.com>
2014-06-27 16:34:05 -0500

    formularios 2

--- backend/lab4/app/views.py
+++ backend/lab4/app/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render_to_response, get_object_or_404
-from django.template import Context
+from django.shortcuts import render_to_response, get_object_or_404, HttpResponseRedirect
+from django.template import Context, RequestContext
 from app.models import Category, Item
 from app.forms import ItemForm
 
@@ -46,4 +46,4 @@ def add_item(request):
 		form = ItemForm()
 
 	context = Context({'title' : 'Adicionar item', 'form' : form})
-	return render_to_response('form.html', context)
+	return render_to_response('form.html', context, context_instance=RequestContext(request))
